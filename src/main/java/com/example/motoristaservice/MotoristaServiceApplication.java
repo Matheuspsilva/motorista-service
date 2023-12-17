c4f3b03c2d04f866f5057bb103607bc5d8d7a44a
Matheus Pereira <matheuspereira.azapfy@gmail.com>
2023-12-17 16:48:12 -0300

    configurando eureka client

--- src/main/java/com/example/motoristaservice/MotoristaServiceApplication.java
+++ src/main/java/com/example/motoristaservice/MotoristaServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.motoristaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotoristaServiceApplication {
 
 	public static void main(String[] args) {
