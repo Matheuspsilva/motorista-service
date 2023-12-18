868febcddcead89fec7940079973032ceccce397
Matheus Pereira <matheuspereira.azapfy@gmail.com>
2023-12-18 18:33:40 -0300

    Integração do Zipkin com o Spring Cloud Sleuth

--- src/main/java/com/example/motoristaservice/controller/MotoristaController.java
+++ src/main/java/com/example/motoristaservice/controller/MotoristaController.java
@@ -2,6 +2,8 @@ package com.example.motoristaservice.controller;
 
 import com.example.motoristaservice.model.Motorista;
 import com.example.motoristaservice.service.MotoristaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import java.util.List;
 @Controller
 public class MotoristaController {
 
+    private static final Logger log = LoggerFactory.getLogger(MotoristaController.class);
     @Autowired
     private MotoristaService motoristaService;
 
@@ -23,27 +26,32 @@ public class MotoristaController {
 
     @GetMapping("/motoristas/{id}")
     public ResponseEntity<Motorista> get(@PathVariable("id") Long id) {
+        log.info("Buscando motorista por id: {}", id);
         return ResponseEntity.ok(motoristaService.get(id));
     }
 
     @GetMapping("/motoristas")
     public ResponseEntity<List<Motorista>> getAll() {
+        log.info("Buscando todos os motoristas");
         return ResponseEntity.ok(motoristaService.getAll());
     }
 
 
     @PostMapping("/motoristas")
     public ResponseEntity<Motorista> create(@RequestBody Motorista motorista) {
+        log.info("Criando motorista: {}", motorista);
         return ResponseEntity.ok(motoristaService.create(motorista));
     }
 
     @PutMapping("/motoristas/{id}")
     public ResponseEntity<Motorista> update(@PathVariable("id") Long id, @RequestBody Motorista motorista) {
+        log.info("Atualizando motorista: {}", motorista);
         return ResponseEntity.ok(motoristaService.update(id, motorista));
     }
 
     @DeleteMapping("/motoristas/{id}")
     public ResponseEntity<Motorista> delete(@PathVariable("id") Long id) {
+        log.info("Deletando motorista por id: {}", id);
         return ResponseEntity.ok(motoristaService.delete(id));
     }
 
--- src/main/java/com/example/motoristaservice/service/MotoristaService.java
+++ src/main/java/com/example/motoristaservice/service/MotoristaService.java
@@ -2,6 +2,8 @@ package com.example.motoristaservice.service;
 
 import com.example.motoristaservice.model.Motorista;
 import com.example.motoristaservice.repository.MotoristaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +12,30 @@ import java.util.List;
 @Service
 public class MotoristaService {
 
+    private static final Logger log = LoggerFactory.getLogger(MotoristaService.class);
     @Autowired
     private MotoristaRepository motoristaRepository;
 
-    public Motorista get(Long id) {
+    public Motorista get(Long id)
+    {
+        log.info("Buscando motorista por id: {}", id);
         return motoristaRepository.findById(id).orElse(null);
     }
 
     public Motorista create(Motorista motorista) {
+        log.info("Criando motorista: {}", motorista);
         return motoristaRepository.save(motorista);
     }
 
 
     public List<Motorista> getAll() {
+        log.info("Buscando todos os motoristas");
         return motoristaRepository.findAll();
     }
 
     public Motorista update(Long id, Motorista motorista) {
+        //show tracer and span
+        log.info("Atualizando motorista: {}", motorista);
         Motorista motoristaSalvo = motoristaRepository.findById(id).orElseThrow(() -> new RuntimeException("Motorista não encontrado"));
         motoristaSalvo.setNome(motorista.getNome());
         motoristaSalvo.setCnh(motorista.getCnh());
@@ -34,6 +43,7 @@ public class MotoristaService {
     }
 
     public Motorista delete(Long id) {
+        log.info("Deletando motorista por id: {}", id);
         Motorista motorista = motoristaRepository.findById(id).orElse(null);
         motoristaRepository.deleteById(id);
         return motorista;
